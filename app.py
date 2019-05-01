c9f7637a9f1ebae53222f81152527f7a920ee907
viraj <anchan.viraj@gmail.com>
2019-05-01 19:05:45 +0530

    add flask app to server the keras model
    add flask app to server the keras model

---
+++ app.py
@@ -0,0 +1,53 @@
+from __future__ import division, print_function
+# coding=utf-8
+
+import io
+
+import numpy as np
+from PIL import Image
+import tensorflow as tf
+from gevent.pywsgi import WSGIServer
+from flask import Flask, request, render_template, jsonify
+
+
+app = Flask('crack_detection')
+
+# Model saved with Keras model.save()
+MODEL_PATH = 'models/crack_detection.h5'
+
+# Load trained model
+model = tf.keras.models.load_model(MODEL_PATH)
+model._make_predict_function()
+print('Model loaded. Start serving...')
+
+
+print('Model loaded. Check http://127.0.0.1:8080/')
+
+
+def predict(img, model):
+    img = img.resize((150, 150))
+    x = tf.keras.preprocessing.image.img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+    images = np.vstack([x])
+    preds = model.predict(images)
+    return preds
+
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
+
+
+@app.route('/predict', methods=['POST'])
+def predict_image_class():
+    img = request.files['file'].read()
+    img = Image.open(io.BytesIO(img))
+    prediction = predict(img, model)
+    class_name = "crack" if prediction[0] < 0.5 else "no_crack"
+    response = {"class": class_name}
+    return jsonify(response)
+
+
+if __name__ == '__main__':
+    http_server = WSGIServer(('', 8080), app)
+    http_server.serve_forever()
