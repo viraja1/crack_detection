d8617d05784ede253c795df5161d846a6b550b84
viraj <anchan.viraj@gmail.com>
2019-05-05 22:36:29 +0530

    add model training changes
    add model training changes

--- models/model.py
+++ models/model.py
@@ -4,14 +4,12 @@ import tensorflow as tf
 
 MODEL_DATA_PATH = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'data')
 
-# training crack images
-train_crack_dir = os.path.join(MODEL_DATA_PATH, 'train/crack')
-
-# training no_crack images
-train_no_crack_dir = os.path.join(MODEL_DATA_PATH, 'train/no_crack')
-
-print('total training crack images:', len(os.listdir(train_crack_dir)))
-print('total training no_crack images:', len(os.listdir(train_no_crack_dir)))
+train_dir = os.path.join(MODEL_DATA_PATH, 'train')
+validation_dir = os.path.join(MODEL_DATA_PATH, 'validation')
+train_length = len(os.listdir(os.path.join(train_dir, 'crack'))) + len(os.listdir(os.path.join(train_dir, 'no_crack')))
+validation_length = len(os.listdir(os.path.join(validation_dir, 'crack'))) + \
+                    len(os.listdir(os.path.join(validation_dir, 'no_crack')))
+batch_size = 300
 
 model = tf.keras.models.Sequential([
  tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(128, 128, 3)),
@@ -35,19 +33,20 @@ model.compile(loss='binary_crossentropy',
 train_datagen = tf.keras.preprocessing.image.ImageDataGenerator(rescale=1/255)
 validation_datagen = tf.keras.preprocessing.image.ImageDataGenerator(rescale=1/255)
 
-train_generator = train_datagen.flow_from_directory(os.path.join(MODEL_DATA_PATH, 'train'),
-                                                    target_size=(128, 128), batch_size=100, class_mode='binary')
+train_generator = train_datagen.flow_from_directory(train_dir,
+                                                    target_size=(128, 128), batch_size=batch_size, class_mode='binary')
 
-validation_generator = train_datagen.flow_from_directory(os.path.join(MODEL_DATA_PATH, 'validation'),
-                                                         target_size=(128, 128), batch_size=10, class_mode='binary')
+validation_generator = train_datagen.flow_from_directory(validation_dir,
+                                                         target_size=(128, 128), batch_size=batch_size,
+                                                         class_mode='binary')
 
 history = model.fit_generator(
       train_generator,
-      steps_per_epoch=8,
-      epochs=20,
+      steps_per_epoch=int(train_length / batch_size),
+      epochs=15,
       verbose=1,
       validation_data=validation_generator,
-      validation_steps=8
+      validation_steps=int(validation_length / batch_size)
 )
 
 model.save(os.path.join(os.path.dirname(__file__), 'crack_detection.h5'))
